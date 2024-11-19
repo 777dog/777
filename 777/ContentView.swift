e0c215cd2b57295c303ada2860f33d3548674938
777dog <aa24.34xc@g.chuo-u.ac.jp>
2024-11-19 17:27:14 +0900

    Initial Commit

---
+++ 777/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  777
+//
+//  Created by 高橋　寛人  on 2024/11/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 777/_77App.swift
@@ -0,0 +1,17 @@
+//
+//  _77App.swift
+//  777
+//
+//  Created by 高橋　寛人  on 2024/11/19.
+//
+
+import SwiftUI
+
+@main
+struct _77App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
